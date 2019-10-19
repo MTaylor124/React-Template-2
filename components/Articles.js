d118f6a97c96fc384e578bd10f65411029de724b
MTaylor124 <matthew_taylor1@student.uml.edu>
2019-10-19 01:29:07 -0400

    Added change password functionality

--- components/Articles.js
+++ components/Articles.js
@@ -1,18 +1,49 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, TextInput } from 'react-native';
 import firebase from 'firebase'
-export default function Articles() {
-  return (
-    <View style={styles.container}>
-      <Text>congrats you buttfucker you logged in</Text>
-      <TouchableOpacity
-      onPress={() => {
-        firebase.auth().signOut()
-      }}>
-        <Text>sign out</Text>
-      </TouchableOpacity>
-    </View>
-  );
+export default class Articles extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      newPassword: ''
+    }
+  }
+  onbuttonPressWarning(newPass) {
+
+  }
+  onButtonPressUpdate(newPass) {
+    const user = firebase.auth().currentUser
+    user.updatePassword(newPass).then(() => {
+      alert('new password changed to', newPass)
+    })
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>congrats you buttfucker you logged in</Text>
+        <TouchableOpacity
+        onPress={() => {
+          firebase.auth().signOut()
+        }}>
+          <Text>sign out</Text>
+        </TouchableOpacity>
+
+        <TextInput
+          placeholder="new password"
+          placeholderTextColor='black'
+          style={styles.newpass}
+          value={this.state.newPassword}
+          onChangeText={(newPassword) => this.setState({newPassword})}
+          />
+          <TouchableOpacity
+            onPress={() => {this.onButtonPressUpdate(this.state.newPassword)}}
+          >
+            <Text style={styles.loginbutton}>Change Password</Text>
+          </TouchableOpacity>
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
